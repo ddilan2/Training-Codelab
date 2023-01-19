b54540057cc6d7f12c088af47c536d2dd41c18f2
ddilan2 <ddilan2@illinois.edu>
2023-01-19 12:09:32 -0600

    Tested virutal enviornment and wrote simple car object for training up to section 1.3.

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,5 +1,16 @@
 # create your class here
 
+class Car:
+    def __init__(self, model, speed):
+        self.model = model
+        self.speed = speed
+
+    def getModel(self):
+        return self.model
+
+    def setSpeed(self, speed):
+        self.speed = speed
+
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
     print(bmw.getModel(), bmw.speed)
