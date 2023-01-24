cf4b993f8b88c8eff1fce6b522d2858c350ab5c4
ddilan2 <ddilan2@illinois.edu>
2023-01-23 22:49:38 -0600

    Completed section 8 of training, javascript buttons.

--- exercises/JavaScript/program.js
+++ exercises/JavaScript/program.js
@@ -1,10 +1,17 @@
 var body;
 
 // implement onload function
-
+window.onload = function() {
+    body = document.querySelector("body");
+}
 
 // implement changeColorBlue
+window.changeColorBlue = function() {
+    body.style.backgroundColor = "#a8ceff";
+}
 
-
-// implement changeColorPurple
+// implement changeColorRed
+window.changeColorRed = function() {
+    body.style.backgroundColor = "#ff0000";
+}
 
